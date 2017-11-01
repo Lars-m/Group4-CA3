6c880e6266e72e5b2b88d705200d180ae02ecce0
Farki93 <cph-if36@cphbusiness.com>
2017-11-01 18:19:30 +0100

    add address page

--- Group4-CA3-WebApp/src/pages/About.js
+++ Group4-CA3-WebApp/src/pages/About.js
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 
 export default class About extends Component{
   render() {
+    fetch('http://localhost:8080/seedMaven/api/address')
+    
     return <h1>About</h1>
   }
 }
---
+++ Group4-CA3-WebApp/src/pages/Address.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import adressData from "../facades/addressFacade";
+
+class Address extends Component {
+
+  constructor() {
+    super();
+    this.state = { data: [], err: "" }
+
+    
+  }
+
+  componentWillMount() {
+    adressData.getData((e, data) => {
+      if (e) {
+        return this.setState({ err: e.err })
+      }
+      this.setState({ err: "", data });
+      
+    });
+  }
+
+
+  render() {
+    const listItems = this.state.data.map((d) => <li key>  {d.city} {d.street} {d.zip} {d.description}</li>);
+
+    return (
+      <div>
+        <h2>Address</h2>
+        <p>This message is fetched from the server if you were properly logged in</p>
+        <div>
+       
+            {listItems}
+        
+          
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Address;
